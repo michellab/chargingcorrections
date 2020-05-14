047372d18f12bd96bb4d080fb5692bd6f4736dfa
Juliette Fenogli <juliette.fenogli@gmail.com>
2020-05-14 18:38:36 +0200

    added the possibility to modify the charges of the solute according to lambda

--- dcd-to-pqr.py
+++ dcd-to-pqr.py
@@ -1,5 +1,25 @@
 import MDAnalysis
 from MDAnalysis.core.topologyattrs import Radii
+from MDAnalysis.core.topologyattrs import Charges
+import MDAnalysis.topology.TOPParser
+import os
+
+#assigning lambda value
+
+print('enter lambda value :')
+
+L = float(input())
+
+
+# towards the automation of the code 
+
+#path = __file__
+#dirpath = os.path.dirname(path)
+#dirname = os.path.basename(dirpath)
+
+#L = float(dirname[7:])
+
+
 
 PRM7 = "input/SYSTEM.top"
 DCD = "input/traj000000001.dcd"
@@ -28,10 +48,29 @@ rad = [radO,radC,radO,radC,radC,radC,radC,radC,radC,radO3,radC,radO4,radC,radH,r
 
 ligand_u.add_TopologyAttr(Radii(rad))            
 
+# how to customize the charges
 
+topo = MDAnalysis.topology.TOPParser.TOPParser("input/SYSTEM.top")
+
+topology_parsed = topo.parse()
+
+tab_charges = topology_parsed.charges.values
+
+first_charges = []
+
+for i in range(len(selection)):
+    first_charges.append(tab_charges[i])
+  
+lambda_charges = []
+
+for charge in first_charges:
+    lambda_charges.append(charge*(1-L))
+
+ligand_u.add_TopologyAttr(Charges(lambda_charges))
+    
 # write pqr file
 
-ligand_u.atoms.write('output/ASA.pqr')
+ligand_u.atoms.write('../../pqr_snapshots/LIG_free_L_'+str(L)+'.pqr')
 
 
 
